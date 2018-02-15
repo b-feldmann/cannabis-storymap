e844181ecb26d6a9e650ca8c3333d846c50ce3cf
Jonas Chromik <jonas.chromik@student.hpi.de>
2018-02-15 11:24:00 +0100

    Add timeline of recreational legalization

--- src/pages/UsaCannabisLegalizedPage/index.js
+++ src/pages/UsaCannabisLegalizedPage/index.js
@@ -5,10 +5,37 @@ import Row from "antd/es/grid/row";
 import Col from "antd/es/grid/col";
 import Card from "antd/es/card/index";
 
+import legalization_dates from './legalization_dates';
 
 import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+import Timeline from 'react-visjs-timeline';
 
 const data = [{ year: 2003, state: 'Alaska', dummy: 1 },{ year: 2017, state: 'FOOBAR', dummy: 1 },{ year: 2008, state: 'Colorado', dummy: 1 }];
+
+const options = {
+  width: '100%',
+  height: '60px',
+  stack: false,
+  showMajorLabels: true,
+  showCurrentTime: true,
+  zoomMin: 1000000,
+  type: 'background',
+  format: {
+    minorLabels: {
+      minute: 'h:mma',
+      hour: 'ha'
+    }
+  }
+};
+
+const items = [
+  {id: 1, content: 'item 1', start: '2013-04-20'},
+  {id: 2, content: 'item 2', start: '2013-04-14'},
+  {id: 3, content: 'item 3', start: '2013-04-18'},
+  {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
+  {id: 5, content: 'item 5', start: '2013-04-25'},
+  {id: 6, content: 'item 6', start: '2013-04-27'}
+];
 
 class CustomTooltip extends Component {
   render() {
@@ -28,6 +55,13 @@ export default class UsaCannabisLegalizedPage extends Component {
     super(props);
 
     this.state = {};
+    this.state.dates = legalization_dates
+      .filter(state => state.recreational != "not")
+      .map(state => ({
+        content: state.state,
+        start: state.recreational
+      }));
+    console.log(this.state.dates)
   }
 
   render() {
@@ -36,11 +70,7 @@ export default class UsaCannabisLegalizedPage extends Component {
         <Row type='flex' gutter={16}>
           <Col span={20} offset={2}>
             <Card title="Legalization of Cannabis over the years" align='justify'>
-              <LineChart width={800} height={100} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-                <XAxis domain={[1996, 2018]} dataKey={'year'} type="number" name='Year' unit=''/>
-                <Line type='monotone' dataKey='dummy' stroke='#8884d8' strokeWidth={2}/>
-                <Tooltip content={<CustomTooltip/>}/>
-              </LineChart>
+              <Timeline items={this.state.dates} />
             </Card>
           </Col>
           <Col span={2}/>
